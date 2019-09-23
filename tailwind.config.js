a0a7372ca10e97dfd60f1be695ef4792284c3194
Casper <me@casperengelmann.com>
2019-09-23 16:29:37 +0200

    added some custom extensions to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      width: {
+        row: 'calc(100% - 0.75rem)',
+        'row-tight': 'calc(100% - 0.25rem)',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
