3930675f8b8114d9df934addc9f7888140fcbef8
Casper <me@casperengelmann.com>
2019-09-23 16:35:34 +0200

    added translations for login/register

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -16,4 +16,25 @@ return [
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
+    'login.title' => 'Login',
+    'login.email' => 'Email',
+    'login.password' => 'Password',
+    'login.remember' => 'Remember me',
+    'login.submit' => 'Login',
+    'login.forgot' => 'Forgot password?',
+    'login.no-account' => 'Not registered yet?',
+    'login.register' => 'Register here',
+
+    'register.title' => 'Register',
+    'register.name' => 'Name',
+    'register.first_name' => 'First name',
+    'register.last_name' => 'Last name',
+    'register.email' => 'Email',
+    'register.phonenumber' => 'Phonenumber',
+    'register.password' => 'Password',
+    'register.password.confirm' => 'Confirm password',
+    'register.submit' => 'Register',
+    'register.already-registered' => 'Already registered?',
+    'register.login' => 'Login',
+
 ];
