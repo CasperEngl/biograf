721743bcea047baa30d2d9afb214343631c00816
Casper <me@casperengelmann.com>
2019-09-26 00:03:07 +0200

    added fillables

--- app/Seat.php
+++ app/Seat.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Seat extends Model
 {
+    protected $fillable = [
+        'cinema_id',
+        'row',
+        'column',
+        'active',
+    ];
+
     public function reservation()
     {
         return $this->hasOne(Reservation::class);
