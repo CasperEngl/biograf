cd33394a50ad4d5dd7aae8cb5be98a21743470ad
Casper <me@casperengelmann.com>
2019-09-26 02:17:09 +0200

    added films table

---
+++ database/migrations/2019_09_25_234652_create_films_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('tmdb_id');
+            $table->string('imdb_id')->nullable();
+            $table->string('category');
+            $table->string('title');
+            $table->string('language');
+            $table->text('overview');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
