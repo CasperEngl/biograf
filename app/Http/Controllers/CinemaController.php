c8233178dae248076bcbc870ea58b783f1d964d1
Casper <me@casperengelmann.com>
2019-09-26 02:17:04 +0200

    removed backend things from frontend controller

--- app/Http/Controllers/CinemaController.php
+++ app/Http/Controllers/CinemaController.php
@@ -4,18 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Seat;
 use App\Tmdb;
+use App\Cinema;
 use Illuminate\Http\Request;
+use App\ViewModels\CinemaViewModel;
 use App\Repositories\CinemaRepository;
 use App\Http\Requests\CinemaStoreRequest;
 
 class CinemaController extends Controller
 {
-    public function __construct(CinemaRepository $cinema, CinemaViewModel $cinemaModel, Seat $seat, Tmdb $tmdb)
+    public function __construct(CinemaRepository $cinema)
     {
         $this->cinema = $cinema;
-        $this->cinemaModel = $cinemaModel;
-        $this->seat = $seat;
-        $this->tmdbRepo = $tmdb->repository();
 
         $this->middleware('auth');
     }
@@ -27,29 +26,8 @@ class CinemaController extends Controller
         ]);
     }
 
-    public function create()
+    public function edit(Cinema $cinema)
     {
-        return view('cinema.create');
-    }
-
-    public function edit()
-    {
-        return view('cinema.create')->with();
-    }
-
-    public function store(CinemaStoreRequest $request)
-    {
-        $cinema = $this->cinema::create($request);
-
-        foreach ($request->seats as $seat) {
-            $cinema->seats()->save(new Seat($seat));
-        }
-
-        return redirect()->route('cinema.index');
-    }
-
-    public function update(CinemaStoreRequest $request)
-    {
-        $this->cinema::update($request);
+        return dd($cinema);
     }
 }
