d712223ce17079a1584fcf76d7c99b70958b2985
Casper <me@casperengelmann.com>
2019-09-27 16:13:01 +0200

    replaced shadow on cards with border + container around cinema maker + cinema name in layout

--- resources/views/cinema/index.blade.php
+++ resources/views/cinema/index.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
       @forelse ($cinemas as $cinema)
         <div class="col w-1/2">
-          <div class="p-4 shadow rounded">
+          <div class="p-4 border-2 border-gray-300 rounded">
             <h2 class="text-2xl mb-2">{{ $cinema->name }}</h2>
             <p class="text-gray-600 mb-2">{{ trans('cinema.seat.count') }} {{ count($cinema->seats) }}</p>
-            <cinema-layout class="mb-4" :cinema-rows="{{ json_encode($cinema->rows()) }}"></cinema-layout>
+            <cinema-layout class="mb-4" cinema-name="{{ $cinema->name }}" :cinema-rows="{{ json_encode($cinema->rows()) }}"></cinema-layout>
             <a href="{{ route('cinema.edit', ['cinema' => $cinema]) }}" class="btn btn-primary">{{ trans('cinema.edit') }}</a>
           </div>
         </div>
@@ -17,6 +17,9 @@
           <h1>{{ trans('cinema.nonefound') }}</h1>
         </div>
       @endforelse
+    </div>
+    <div class="container py-8">
+      <cinema-maker></cinema-maker>
     </div>
   </div>
 @endsection
