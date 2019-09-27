63d1a01eeadbdb384a44d326f976d8bde84f283b
Casper <me@casperengelmann.com>
2019-09-27 16:10:43 +0200

    recreated cinema maker

--- resources/js/components/CinemaMaker.vue
+++ resources/js/components/CinemaMaker.vue
@@ -1,77 +1,90 @@
 <template>
-  <section class="container">
-    <div class="row">
+  <section>
+    <div class="row mb-4">
+      <div class="col w-full">
+        <label for="name">
+          <span class="block text-gray-700 text-sm font-bold mb-2">Navn</span>
+          <input type="text" class="field" v-model="name">
+        </label>
+      </div>
+    </div>
+    <div class="row mb-4">
       <div class="col w-1/2">
         <label for="rows">
-          <span class="w-full block">Rows</span>
-          <input type="number" v-model.number="rows" id="rows" class="w-full" />
+          <span class="block text-gray-700 text-sm font-bold mb-2">Rækker</span>
+          <select class="field" name="rows" id="rows" v-model.number="rowCount">
+            <option :value="option" v-for="option in 20" :key="option">{{ option }}</option>
+          </select>
         </label>
       </div>
       <div class="col w-1/2">
         <label for="columns">
-          <span class="w-full block">Columns</span>
-          <input
-            type="number"
-            v-model.number="columns"
-            id="columns"
-            class="w-full"
-          />
+          <span class="block text-gray-700 text-sm font-bold mb-2">Kolonner</span>
+          <select class="field" name="columns" id="columns" v-model.number="columnCount">
+            <option :value="option" v-for="option in 20" :key="option">{{ option }}</option>
+          </select>
         </label>
       </div>
     </div>
-    <table class="table-fixed my-6">
-      <tbody>
-        <tr class="table-row" v-for="(row, rowIndex) in rows" :key="rowIndex">
-          <td colspan="2" class="table-cell pr-3">Row {{ rowIndex + 1 }}</td>
-          <td colspan="2" class="table-cell" v-for="(column, columnIndex) in columns" :key="columnIndex">
-            <input
-              @click="selectSeat"
-              type="checkbox"
-              :id="`${rowIndex}-${columnIndex}`"
-              :value="`${rowIndex}-${columnIndex}`"
-            />
-            <label :for="`${rowIndex}-${columnIndex}`"></label>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <cinema-layout :cinema-name="name" :cinema-rows="rows" :disabled="false" :inputs-disabled="false"></cinema-layout>
+    <button type="submit" class="btn btn-primary mt-4" @click="() => submitting = true">Opret biograf</button>
   </section>
 </template>
 
 <script>
+import groupBy from 'lodash-es/groupBy';
+
+function reverseObject(obj) {
+  return Object.keys(obj).sort().reverse().reduce((result, key) => {
+    const ref = result;
+
+    ref[key] = obj[key];
+
+    return ref;
+  }, {});
+}
+
 export default {
   data: () => ({
-    rows: 8,
-    columns: 10,
+    name: '',
+    rowCount: 8,
+    columnCount: 10,
+    rows: {},
+    seats: [],
+    submitting: false,
   }),
+  created() {
+    this.renderSeats(this.rowCount, this.columnCount);
+  },
+  watch: {
+    rowCount() {
+      this.renderSeats(this.rowCount, this.columnCount);
+    },
+    columnCount() {
+      this.renderSeats(this.rowCount, this.columnCount);
+    },
+  },
   methods: {
-    selectSeat(e) {
-      console.log(e.target);
+    renderSeats(rowCount, columnCount) {
+      this.rows = {};
+      this.seats = [];
+
+      const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+      for (let row = 0; row < rowCount; row++) {
+        const letter = alphabet[row];
+
+        for (let column = 0; column < columnCount; column++) {
+          this.seats.push({
+            row: letter,
+            column,
+            active: true,
+          });
+        }
+      }
+
+      this.rows = reverseObject(groupBy(this.seats, 'row'));
     },
   },
 };
 </script>
-
-<style scoped>
-tr {
-  margin: 0.25rem 0;
-  width: 100%;
-  display: inline-block;
-}
-
-input[type="checkbox"] {
-  display: none;
-}
-
-input[type="checkbox"] + label {
-  margin: 0.1rem;
-  width: 24px;
-  height: 20px;
-  display: inline-block;
-  background: #222;
-}
-
-input[type="checkbox"]:checked + label {
-  background: yellow;
-}
-</style>
