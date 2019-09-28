e1ea3d8d82910420fa8c446c63a4353c9ae86d31
Casper <me@casperengelmann.com>
2019-09-28 23:40:01 +0200

    added casts + row ids + parent cinema

--- app/Seat.php
+++ app/Seat.php
@@ -2,16 +2,31 @@
 
 namespace App;
 
+use App\Cinema;
+use App\Reservation;
 use Illuminate\Database\Eloquent\Model;
 
 class Seat extends Model
 {
+    const ROW_IDS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
     protected $fillable = [
         'cinema_id',
         'row',
         'column',
         'active',
     ];
+
+    protected $casts = [
+        'row' => 'string',
+        'column' => 'integer',
+        'active' => 'boolean',
+    ];
+
+    public function cinema()
+    {
+        return $this->belongsTo(Cinema::class);
+    }
 
     public function reservation()
     {
