c2ab671dcf83ca6eae3fe81d89de07e5c6dd788e
Casper <me@casperengelmann.com>
2019-09-28 23:41:23 +0200

    added proper screen light

--- resources/js/components/CinemaLayout.vue
+++ resources/js/components/CinemaLayout.vue
@@ -1,23 +1,30 @@
 <template>
   <section class="relative p-4 bg-gray-900">
-    <div class="w-full min-h-8 bg-white" ref="screen" :style="{
-      width: `calc(100% - ${seatWidth / 2}px)`,
-      marginLeft: `${seatWidth / 2}px`,
+    <div class="relative w-full min-h-12 flex items-center justify-center bg-white" ref="screen" :style="{
+      width: 'calc(90% - 25px)',
+      marginLeft: 'calc(5% + 25px)',
     }">
       <h3 class="text-xl text-center py-2" v-if="cinemaName">{{ cinemaName }}</h3>
+      <div :style="{
+        position: 'absolute',
+        top: '48px',
+        height: '200px',
+        width: '100%',
+        background: 'linear-gradient(to bottom, rgba(255, 255, 255, .1) 0%, transparent 100%)',
+        clipPath: 'polygon(5% 0%, 95% 0%, 100% 100%, 0% 100%)',
+      }"></div>
     </div>
-    <div :style="screenShadow"></div>
-    <table class="w-full mt-8">
-      <col width="1">
+    <table class="w-full mt-16 table-fixed">
+      <col width="25">
       <thead>
         <tr>
-          <th ref="walkway"></th>
-          <th class="text-gray-600 font-bold text-sm" v-for="(seat, index) in singleRow" :key="index" ref="seat">{{ seat.column }}</th>
+          <th></th>
+          <th class="pb-1 text-gray-600 font-bold text-sm" v-for="(seat, index) in singleRow" :key="index" ref="seat">{{ seat.column }}</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(row, letter) in rows" :key="letter">
-          <td class="text-gray-600 font-bold text-sm" :style="{
+          <td class="text-gray-600 font-bold text-sm text-center" :style="{
             paddingRight: `${seatWidth / 4}px`
           }" v-if="letter">{{ letter.toUpperCase() }}</td>
           <td v-for="(seat, index) in row" :key="index">
@@ -45,8 +52,6 @@
 </template>
 
 <script>
-import debounce from 'lodash-es/debounce';
-
 export default {
   props: {
     cinemaName: {
@@ -68,14 +73,6 @@ export default {
       default: true,
     },
   },
-  created() {
-    this.updateScreenShadow();
-
-    document.addEventListener('resize', debounce(this.updateScreenShadow, 250));
-  },
-  destroyed() {
-    document.removeEventListener('resize', debounce(this.updateScreenShadow, 250));
-  },
   data: (vm) => ({
     rows: vm.cinemaRows,
     screenShadow: {},
@@ -87,24 +84,6 @@ export default {
       const ref = seat;
 
       ref.active = seat.active ? 0 : 1;
-    },
-    updateScreenShadow() {
-      this.$nextTick().then(() => {
-        this.screenWidth = this.$refs.screen.clientWidth;
-        this.seatWidth = this.$refs.seat[0].clientWidth;
-
-        this.screenShadow = {
-          pointerEvents: 'none',
-          position: 'absolute',
-          right: `${this.seatWidth / 4}px`,
-          marginTop: '-1px',
-          marginLeft: `${this.seatWidth / 2}px`,
-          borderWidth: `0 ${this.screenWidth / 2 - this.seatWidth / 4}px ${this.screenWidth / 10}px`,
-          borderImage: 'linear-gradient(to bottom, rgba(255, 255, 255, .1), transparent) 100% / 1.1 / 0 stretch',
-          height: 0,
-          width: `${this.screenWidth - this.seatWidth}px`,
-        };
-      });
     },
   },
   watch: {
