0879f504011b10b6bed303601924351cb7a66978
Casper <me@casperengelmann.com>
2019-09-28 23:43:13 +0200

    put cinema maker at the top

--- resources/views/cinema/index.blade.php
+++ resources/views/cinema/index.blade.php
@@ -2,10 +2,15 @@
 
 @section('content')
   <div class="container py-8">
+    <div class="row py-8">
+      <div class="col w-full">
+        <cinema-maker></cinema-maker>
+      </div>
+    </div>
     <div class="row">
       @forelse ($cinemas as $cinema)
-        <div class="col w-1/2">
-          <div class="p-4 border-2 border-gray-300 rounded">
+        <div class="col w-1/2 my-3">
+          <div class="p-4 h-full border-2 border-gray-300 rounded">
             <h2 class="text-2xl mb-2">{{ $cinema->name }}</h2>
             <p class="text-gray-600 mb-2">{{ trans('cinema.seat.count') }} {{ count($cinema->seats) }}</p>
             <cinema-layout class="mb-4" cinema-name="{{ $cinema->name }}" :cinema-rows="{{ json_encode($cinema->rows()) }}"></cinema-layout>
@@ -17,9 +22,6 @@
           <h1>{{ trans('cinema.nonefound') }}</h1>
         </div>
       @endforelse
-    </div>
-    <div class="container py-8">
-      <cinema-maker></cinema-maker>
     </div>
   </div>
 @endsection
