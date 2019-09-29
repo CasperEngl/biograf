587278f3ef31156e779b7843178ee85453961bcc
Casper <me@casperengelmann.com>
2019-09-29 04:37:07 +0200

    added basic film controller that only outputs films as json

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Film;
 use Illuminate\Http\Request;
 
 class FilmController extends Controller
 {
-    //
+    public function __construct(Film $film)
+    {
+        $this->film = $film;
+    }
+
+    public function index()
+    {
+        return $this->film->all();
+    }
+
+    public function show(Request $request)
+    {
+        return $this->film->find($request->film);
+    }
 }
