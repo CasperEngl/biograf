739a3414ecfe11c7af1e59abb9f5e6332e511007
Casper <me@casperengelmann.com>
2019-09-29 04:36:37 +0200

    added image helper

--- app/Tmdb.php
+++ app/Tmdb.php
@@ -5,21 +5,32 @@ namespace App;
 use Tmdb\Client;
 use Tmdb\ApiToken;
 use Tmdb\Repository\MovieRepository;
+use Tmdb\Repository\ConfigurationRepository;
 
-class Tmdb {
-  public function __construct()
-  {
-    $this->apiToken = new ApiToken(env('TMDB_API_KEY'));
-    $this->client = new Client($this->apiToken);
-  }
+class Tmdb
+{
+    public function __construct()
+    {
+        $this->apiToken = new ApiToken(env('TMDB_API_KEY'));
+        $this->client = new Client($this->apiToken);
 
-  public function client()
-  {
-    return $this->client;
-  }
+        $configRepository = new ConfigurationRepository($this->client);
+        
+        $this->config = $configRepository->load();
+    }
 
-  public function repository()
-  {
-    return new MovieRepository($this->client);
-  }
+    public function client()
+    {
+        return $this->client;
+    }
+
+    public function repository()
+    {
+        return new MovieRepository($this->client);
+    }
+
+    public function imageHelper()
+    {
+        return new \Tmdb\Helper\ImageHelper($this->config);
+    }
 }
