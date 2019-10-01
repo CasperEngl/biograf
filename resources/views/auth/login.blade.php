7a3655b0ab7b3d206149320ca9f1929bae756e4a
Casper <me@casperengelmann.com>
2019-10-01 03:11:58 +0200

    redid layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,60 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mx-auto">
-        <h1 class="text-5xl uppercase font-bold">{{ trans('auth.login.title') }}</h1>
-
-        <form class="w-full py-6" method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div class="flex flex-wrap mb-6">
-                <input-label for="email">{{ trans('auth.login.email') }}</input-label>
-                <input-field id="email" type="email" name="email" value="{{ old('email') }}" required autofocus></input-field>
-
-                @if ($errors->has('email'))
-                    <p class="text-red-500 text-xs italic mt-4">
-                        {{ $errors->first('email') }}
-                    </p>
-                @endif
-            </div>
-
-            <div class="flex flex-wrap mb-6">
-                <input-label for="password">{{ trans('auth.login.password') }}</input-label>
-                <input-field id="password" type="password" class="{{ $errors->has('password') ? ' border-red-500' : '' }}" name="password" required></input-field>
-
-                @if ($errors->has('password'))
-                    <p class="text-red-500 text-xs italic mt-4">
-                        {{ $errors->first('password') }}
-                    </p>
-                @endif
-            </div>
-
-            <div class="flex mb-6">
-                <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                <label class="text-sm text-gray-700 ml-3" for="remember">
-                    {{ trans('auth.login.remember') }}
-                </label>
-            </div>
-
-            <div class="flex flex-wrap items-center">
-                <button type="submit" class="btn btn-primary">{{ trans('auth.login.submit') }}</button>
-
-                @if (Route::has('password.request'))
-                    <a class="text-sm text-blue-500 hover:text-blue-700 whitespace-no-wrap no-underline ml-auto" href="{{ route('password.request') }}">
-                        {{ trans('auth.login.forgot') }}
-                    </a>
-                @endif
-
-                @if (Route::has('register'))
-                    <p class="w-full text-xs text-center text-gray-700 mt-8 -mb-4">
-                        {{ trans('auth.login.no-account') }}
-                        <a class="text-blue-500 hover:text-blue-700 no-underline" href="{{ route('register') }}">
-                            {{ trans('auth.login.register') }}
+<div class="container py-32">
+    <div class="row py-6">
+        <div class="col w-1/2">
+            <h1 class="text-5xl mb-4 uppercase font-bold">{{ trans('auth.login.title') }}</h1>
+            <h2 class="text-2xl text-gray-600">{{ trans('auth.login.subtitle') }}</h2>
+        </div>
+        <div class="col w-1/2">
+            <form class="w-full" method="POST" action="{{ route('login') }}">
+                @csrf
+    
+                <div class="row my-3">
+                    <div class="col w-full">
+                        <input-label for="email">{{ trans('auth.login.email') }}</input-label>
+                        <input-field id="email" type="text" name="email" value="{{ old('email') }}" required autofocus></input-field>
+                        <error-message key="email"></error-message>
+                    </div>
+                </div>
+                <div class="row my-3">
+                    <div class="col w-full">
+                        <input-label for="password">{{ trans('auth.login.password') }}</input-label>
+                        <input-field id="password" type="password" class="{{ $errors->has('password') ? ' border-red-500' : '' }}" name="password" required></input-field>
+                        <error-message key="password"></error-message>
+                    </div>
+                </div>
+    
+                <div class="flex mb-6">
+                    <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+    
+                    <label class="text-sm text-gray-700 ml-3" for="remember">
+                        {{ trans('auth.login.remember') }}
+                    </label>
+                </div>
+    
+                <div class="flex flex-wrap items-center">
+                    <button type="submit" class="btn btn-primary">{{ trans('auth.login.submit') }}</button>
+    
+                    @if (Route::has('password.request'))
+                        <a class="text-sm text-blue-500 hover:text-blue-700 whitespace-no-wrap no-underline ml-auto" href="{{ route('password.request') }}">
+                            {{ trans('auth.login.forgot') }}
                         </a>
-                    </p>
-                @endif
-            </div>
-        </form>
+                    @endif
+    
+                    @if (Route::has('register'))
+                        <p class="w-full text-xs text-center text-gray-700 mt-8 -mb-4">
+                            {{ trans('auth.login.no-account') }}
+                            <a class="text-blue-500 hover:text-blue-700 no-underline" href="{{ route('register') }}">
+                                {{ trans('auth.login.register') }}
+                            </a>
+                        </p>
+                    @endif
+                </div>
+            </form>
+        </div>
     </div>
+</div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,65 +1,71 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1 class="text-5xl uppercase font-bold">{{ trans('auth.register.title') }}</h1>
-
-        <form class="w-full py-6" method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <div class="row my-3">
-                <div class="col w-1/2">
-                    <input-label for="firstname">{{ trans('auth.register.firstname') }}</input-label>
-                    <input-field id="firstname" type="text" class="{{ $errors->has('firstname') ? ' border-red-500' : '' }}" name="firstname" value="{{ old('firstname') }}" required autofocus></input-field>
-
-                    <error-message key="firstname"></error-message>
-                </div>
-                <div class="col w-1/2">
-                    <input-label for="lastname">{{ trans('auth.register.lastname') }}</input-label>
-                    <input-field id="lastname" type="text" class="{{ $errors->has('lastname') ? ' border-red-500' : '' }}" name="lastname" value="{{ old('lastname') }}" required autofocus></input-field>
-
-                    <error-message key="lastname"></error-message>
-                </div>
-            </div>
-
-            <div class="row my-3">
-                <div class="col w-1/2">
-                    <input-label for="email">{{ trans('auth.register.email') }}</input-label>
-                    <input-field id="email" type="email" class="{{ $errors->has('email') ? ' border-red-500' : '' }}" name="email" value="{{ old('email') }}" required></input-field>
-
-                    <error-message key="email"></error-message>
-                </div>
-                <div class="col w-1/2">
-                    <input-label for="phonenumber">{{ trans('auth.register.phonenumber') }}</input-label>
-                    <input-field id="phonenumber" type="tel" class="{{ $errors->has('phonenumber') ? ' border-red-500' : '' }}" name="phonenumber" value="{{ old('phonenumber') }}" required></input-field>
-
-                    <error-message key="phonenumber"></error-message>
-                </div>
-            </div>
-
-            <div class="row my-3">
-                <div class="col w-1/2">
-                    <input-label for="password">{{ trans('auth.register.password') }}</input-label>
-                    <input-field id="password" type="password" class-name="{{ $errors->has('password') ? ' border-red-500' : '' }}" name="password" required></input-field>
+<div class="container py-32">
+    <div class="row py-6">
+        <div class="col w-1/2">
+            <h1 class="text-5xl mb-4 uppercase font-bold">{{ trans('auth.register.title') }}</h1>
+            <h2 class="text-2xl text-gray-600">{{ trans('auth.login.subtitle') }}</h2>
+        </div>
+        <div class="col w-1/2">
+            <form class="w-full" method="POST" action="{{ route('register') }}">
+                @csrf
     
-                    <error-message key="password"></error-message>
+                <div class="row my-3">
+                    <div class="col w-1/2">
+                        <input-label for="firstname">{{ trans('auth.register.firstname') }}</input-label>
+                        <input-field id="firstname" type="text" class="{{ $errors->has('firstname') ? ' border-red-500' : '' }}" name="firstname" value="{{ old('firstname') }}" required autofocus></input-field>
+    
+                        <error-message key="firstname"></error-message>
+                    </div>
+                    <div class="col w-1/2">
+                        <input-label for="lastname">{{ trans('auth.register.lastname') }}</input-label>
+                        <input-field id="lastname" type="text" class="{{ $errors->has('lastname') ? ' border-red-500' : '' }}" name="lastname" value="{{ old('lastname') }}" required autofocus></input-field>
+    
+                        <error-message key="lastname"></error-message>
+                    </div>
                 </div>
-                <div class="col w-1/2">
-                    <input-label for="password-confirm">{{ trans('auth.register.password.confirm') }}</input-label>
-                    <input-field id="password-confirm" type="password" name="password_confirmation" required></input-field>
+    
+                <div class="row my-3">
+                    <div class="col w-1/2">
+                        <input-label for="email">{{ trans('auth.register.email') }}</input-label>
+                        <input-field id="email" type="email" class="{{ $errors->has('email') ? ' border-red-500' : '' }}" name="email" value="{{ old('email') }}" required></input-field>
+    
+                        <error-message key="email"></error-message>
+                    </div>
+                    <div class="col w-1/2">
+                        <input-label for="phonenumber">{{ trans('auth.register.phonenumber') }}</input-label>
+                        <input-field id="phonenumber" type="tel" class="{{ $errors->has('phonenumber') ? ' border-red-500' : '' }}" name="phonenumber" value="{{ old('phonenumber') }}" required></input-field>
+    
+                        <error-message key="phonenumber"></error-message>
+                    </div>
                 </div>
-            </div>
-
-            <div class="row my-3">
-                <div class="col w-full">
-                    <button type="submit" class="btn btn-primary">{{ trans('auth.register.submit') }}</button>
-
-                    <p class="w-full text-xs text-center text-gray-700 mt-8 -mb-4">
-                        {{ trans('auth.register.already-registered') }}
-                        <a class="text-blue-500 hover:text-blue-700 no-underline" href="{{ route('login') }}">{{ trans('auth.register.login') }}</a>
-                    </p>
+    
+                <div class="row my-3">
+                    <div class="col w-1/2">
+                        <input-label for="password">{{ trans('auth.register.password') }}</input-label>
+                        <input-field id="password" type="password" class-name="{{ $errors->has('password') ? ' border-red-500' : '' }}" name="password" required></input-field>
+        
+                        <error-message key="password"></error-message>
+                    </div>
+                    <div class="col w-1/2">
+                        <input-label for="password-confirm">{{ trans('auth.register.password.confirm') }}</input-label>
+                        <input-field id="password-confirm" type="password" name="password_confirmation" required></input-field>
+                    </div>
                 </div>
-            </div>
-        </form>
+    
+                <div class="row my-3">
+                    <div class="col w-full">
+                        <button type="submit" class="btn btn-primary">{{ trans('auth.register.submit') }}</button>
+    
+                        <p class="w-full text-xs text-center text-gray-700 mt-8 -mb-4">
+                            {{ trans('auth.register.already-registered') }}
+                            <a class="text-blue-500 hover:text-blue-700 no-underline" href="{{ route('login') }}">{{ trans('auth.register.login') }}</a>
+                        </p>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
+</div>
 @endsection
