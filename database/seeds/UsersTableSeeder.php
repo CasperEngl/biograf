0f4a799092024dc67cb7f286cb090cc7846a5f55
Casper <me@casperengelmann.com>
2019-10-01 04:32:28 +0200

    add my user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        factory(App\User::class)->create([
+            'firstname' => 'Casper',
+            'lastname' => 'Engelmann',
+            'email' => 'me@casperengelmann.com',
+            'phonenumber' => '40413916',
+            'password' => bcrypt('1234'),
+        ]);
+        
         factory(App\User::class, 10)->create();
     }
 }
