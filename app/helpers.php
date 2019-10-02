940eafb9c063e2d044858ae840e82208c4d6a79a
Casper <me@casperengelmann.com>
2019-10-02 22:02:30 +0200

    added docblocks

--- app/helpers.php
+++ app/helpers.php
@@ -1,40 +1,65 @@
 <?php
 
-use App\Tmdb;
-use Illuminate\Support\Str;
-
 if (! function_exists('title_case')) {
+    /**
+     * Helper function for Str::plural
+     *
+     * @param string $string
+     *
+     * @return \Illuminate\Support\Str
+     */
     function title_case(string $string)
     {
-        return Str::title($string);
+        return \Illuminate\Support\Str::title($string);
     }
 }
 
 if (! function_exists('plural')) {
+    /**
+     * Helper function for Str::plural
+     *
+     * @param string $string
+     *
+     * @return \Illuminate\Support\Str
+     */
     function plural(string $string)
     {
-        return Str::plural($string);
+        return \Illuminate\Support\Str::plural($string);
     }
 }
 
 if (! function_exists('tmdb')) {
+    /**
+     * Helper function for TMDB Client
+     * @return \Tmdb\Client
+     */
     function tmdb()
     {
-        return (new Tmdb)->client();
+        return (new \App\Tmdb)->client;
     }
 }
 
 if (! function_exists('tmdb_repo')) {
+    /**
+     * Helper function for TMDB Movie Repository
+     *
+     * @return \Tmdb\Repository\MovieRepository
+     */
     function tmdb_repo()
     {
-        return (new Tmdb)->repository();
+        return (new \App\Tmdb)->repository();
     }
 }
 
 if (! function_exists('tmdb_image')) {
+    /**
+     * Helper function for TMDB Image Helper
+     *
+     * @return \Tmdb\Helper\ImageHelper
+     */
     function tmdb_image()
     {
-        return (new Tmdb)->imageHelper();
+        return (new \App\Tmdb)->imageHelper();
     }
 }
 
