7052ed4e92242f130168bebe52b95033746c31df
Casper <me@casperengelmann.com>
2019-10-02 22:06:14 +0200

    added more translations

--- resources/lang/en/film.php
+++ resources/lang/en/film.php
@@ -2,4 +2,12 @@
 
 return [
   'read-more' => 'Read more',
+  'trailer' => 'Play trailer',
+  'overview' => 'Overview',
+  'premiere' => 'Premiere',
+  'director' => 'Director',
+  'genre' => 'Genre',
+  'cast' => 'Cast',
+  'previous' => 'Previous',
+  'next' => 'Next',
 ];
