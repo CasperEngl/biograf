685d702e6400cd8744b7a8f86aaee832414e69ff
Casper <me@casperengelmann.com>
2019-10-10 22:11:34 +0200

    added link to image and title

--- resources/views/showing/index.blade.php
+++ resources/views/showing/index.blade.php
@@ -5,17 +5,17 @@
   <div class="row">
     @foreach ($films as $film)
       <div class="col w-1/4 my-3">
-        <figure class="block">
-          <img src="{{ $film->getFirstMediaUrl('backdrop', 'small') }}" alt="{{ $film->title }}">
-        </figure>
-        <a href="{{ route('film.show', compact('film')) }}" class="overflow-hidden relative py-3 block">
-          <div class="absolute inset-0 z-10 w-full" style="background: linear-gradient(transparent 70%, rgba(26, 32, 44, 1) 90%); pointer-events: none;"></div>
-          <h3 class="mb-2 text-2xl uppercase font-black">{{ Str::limit($film->title, 18) }}</h3>
+        <a href="{{ route('film.show', ['slug' => $film->slug]) }}" class="overflow-hidden relative py-3 block">
+          {{-- <div class="absolute inset-0 z-10 w-full" style="background: linear-gradient(transparent 70%, rgba(26, 32, 44, 1) 90%); pointer-events: none;"></div> --}}
+          <figure class="block mb-2">
+            <img src="{{ $film->getFirstMediaUrl('backdrop', 'small') }}" alt="{{ $film->title }}">
+          </figure>
+          <h3 class="text-2xl uppercase font-black">{{ Str::limit($film->title, 18) }}</h3>
         </a>
-        @if (count($film->todaysShowings()))
-          @foreach ($film->todaysShowings() as $showing)
+        @if (count($film->showings))
+          @foreach ($film->showings as $showing)
             <a href="{{ route('showing.show', compact('date', 'showing')) }}" class="relative my-1 h-12 bg-gray-700 flex justify-between">
-              <p class="p-1 w-full flex items-center justify-center">{{ getNearestTimeRoundedUpWithMinimum($showing->start, 5, 1)->format('H:i') }}</p>
+              <p class="p-1 w-full flex items-center justify-center">{{ getNearestTimeRoundedUpWithMinimum($showing->start, 5)->format('H:i') }}</p>
               <span class="absolute inset-y-0 right-0 text-center block overflow-hidden flex items-center justify-center flex-no-wrap h-8 text-sm bg-orange-500 p-px w-12" style="transform: rotate(90deg) translate(8px, -7px);">{{ $showing->cinema->name }}</span>
             </a>
           @endforeach
