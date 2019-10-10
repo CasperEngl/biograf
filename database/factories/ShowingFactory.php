570ac9ea8697b0eacd51b21bd3c95aa5fbb81834
Casper <me@casperengelmann.com>
2019-10-10 22:02:23 +0200

    fixed date crossing over midnight

--- database/factories/ShowingFactory.php
+++ database/factories/ShowingFactory.php
@@ -7,11 +7,26 @@ use Carbon\Carbon;
 use Faker\Generator as Faker;
 
 $factory->define(Showing::class, function (Faker $faker) {
-    $date = $faker->dateTimeBetween(now(), now()->addDays(30));
+    $morning = Carbon::create(now()->year, now()->month, now()->day, 8, 0, 0); //set time to 08:00
+    $evening = Carbon::create(now()->year, now()->month, now()->day, 16, 0, 0); //set time to 16:00
+
+    if(now()->between($morning, $evening, true)) {
+        $date = $faker->dateTimeBetween(now(), now()->addDays(30));
+    } else {
+        $date = $faker->dateTimeBetween($morning, now()->addDays(30));
+    }
+
+    $versions = [
+        '2D',
+        '3D',
+        'IMAX 2D',
+        'IMAX 3D',
+    ];
 
     return [
         'cinema_id' => App\Cinema::all()->random(),
         'film_id' => App\Film::all()->random(),
+        'version' => $faker->randomElement($versions),
         'price' => 10000,
         'date' => Carbon::instance($date),
         'start' => Carbon::instance($date)->addHours(4),
