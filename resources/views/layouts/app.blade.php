874e192cd096ed21647e4ad1da9d49a8503d737f
Casper <me@casperengelmann.com>
2019-10-24 19:31:59 +0200

    biggest update in footer + laravel i18n

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-
+    
+    <script src="/js/lang.js"></script>
     @routes
 </head>
 <body class="bg-gray-900 text-white h-screen antialiased leading-none">
@@ -18,13 +19,13 @@
                 <div class="flex items-center justify-center">
                     <div class="mr-6">
                         <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
-                            @svg('logo', 'inline-block max-h-12')
+                            @svg('logo', 'inline-block h-10 w-10 mr-2')
                             {{ config('app.name', 'Laravel') }}
                         </a>
                     </div>
                     <div class="flex-1 flex items-center justify-end">
-                        <a href="{{ route('showing.pick') }}" class="p-3 border-t-2 border-b-2 border-red-600 hover:border-red-700 rounded-none uppercase font-bold">{{ trans('showing.pick') }}</a>
-                        <a href="{{ route('film.pick') }}" class="p-3 border-t-2 border-b-2 border-red-600 hover:border-red-700 rounded-none uppercase font-bold">{{ trans('film.pick') }}</a>
+                        <a href="{{ route('showing.pick') }}" class="p-3 border-t-2 border-b-2 border-red-600 hover:border-red-700 focus:bg-red-700 rounded-none uppercase font-bold">{{ trans('showing.pick') }}</a>
+                        <a href="{{ route('film.pick') }}" class="p-3 border-t-2 border-b-2 border-red-600 hover:border-red-700 focus:bg-red-700 rounded-none uppercase font-bold">{{ trans('film.pick') }}</a>
                         @guest
                             <a class="no-underline hover:underline text-gray-300 text-sm font-bold uppercase p-3" href="{{ route('login') }}">{{ trans('auth.login') }}</a>
                             @if (Route::has('register'))
@@ -43,29 +44,37 @@
             </div>
         </nav>
 
-        <main class="content">
+        <main class="main-content">
             @yield('content')
         </main>
         
         <footer class="py-16 min-h-md flex items-center" style="background: rgba(0,0,0,0.75);">
             <div class="container">
-                <h2 class="mb-8 text-6xl uppercase text-center font-black">{{ config('app.name') }}</h2>
-                <div class="row items-center">
+                <div class="row">
                     <div class="col w-full md:w-1/3">
+                        <h3 class="mb-4 text-3xl uppercase font-black">{{ trans('footer.upcoming') }}</h3>
                         <nav>
-                            <a href="{{ route('showing.index', ['date' => now()->toDateString()]) }}" class="py-3 block text-xl hover:text-gray-300"><i class="fa fa-caret-right pr-2"></i>{{ trans('showing.all') }}</a>
-                            <a href="{{ route('film.index') }}" class="py-3 block text-xl hover:text-gray-300"><i class="fa fa-caret-right pr-2"></i>{{ trans('film.all') }}</a>
+                            @foreach (App\Film::where('categories', 'like', '%"upcoming"%')->latest()->take(5)->get() as $film)
+                                <a href="{{ route('film.show', ['slug' => $film->slug]) }}" class="py-3 block text-xl hover:text-gray-300">{{ $film->title }}</a>
+                            @endforeach
                         </nav>
                     </div>
                     <div class="col w-full md:w-1/3">
+                        <h3 class="mb-4 text-3xl uppercase font-black">{{ trans('footer.now-playing') }}</h3>
                         <nav>
-                            <a href="{{ route('register') }}" class="py-3 block text-xl hover:text-gray-300"><i class="fa fa-caret-right pr-2"></i>{{ trans('auth.register') }}</a>
-                            <a href="{{ route('login') }}" class="py-3 block text-xl hover:text-gray-300"><i class="fa fa-caret-right pr-2"></i>{{ trans('auth.login') }}</a>
+                            @foreach (App\Film::where('categories', 'like', '%"now-playing"%')->take(5)->get() as $film)
+                                <a href="{{ route('film.show', ['slug' => $film->slug]) }}" class="py-3 block text-xl hover:text-gray-300">{{ $film->title }}</a>
+                            @endforeach
                         </nav>
                     </div>
                     <div class="col w-full md:w-1/3">
+                        <h3 class="mb-4 text-3xl uppercase font-black">{{ trans('footer.quicklinks') }}</h3>
                         <nav>
-                            <a href="https://www.themoviedb.org/" class="py-3 block text-xl hover:text-gray-300"><i class="fa fa-caret-right pr-2"></i>themoviedb.org</a>
+                            <a href="{{ route('register') }}" class="py-3 block text-xl hover:text-gray-300">{{ trans('auth.register') }}</a>
+                            <a href="{{ route('login') }}" class="py-3 block text-xl hover:text-gray-300">{{ trans('auth.login') }}</a>
+                            <a href="{{ route('showing.index', ['date' => now()->toDateString()]) }}" class="py-3 block text-xl hover:text-gray-300">{{ trans('showing.all') }}</a>
+                            <a href="{{ route('film.index') }}" class="py-3 block text-xl hover:text-gray-300">{{ trans('film.all') }}</a>
+                            <a href="https://www.themoviedb.org/" class="py-3 block text-xl hover:text-gray-300">themoviedb.org</a>
                         </nav>
                     </div>
                     <div class="col w-full flex justify-center">
