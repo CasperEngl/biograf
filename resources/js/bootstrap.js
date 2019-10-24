91399b3b4da854080d2ab6b7c0bcd55a03859e2b
Casper <me@casperengelmann.com>
2019-10-24 19:21:16 +0200

    added pusher/echo

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,19 @@
 import axios from 'axios';
+import Pusher from 'pusher-js';
+import Echo from 'laravel-echo';
 
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('[name="csrf-token"]').getAttribute('content');
+
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+  broadcaster: 'pusher',
+  key: 'eeb63050d73b3c199ba3',
+  cluster: 'eu',
+  forceTLS: true,
+});
+
+Pusher.logToConsole = true;
