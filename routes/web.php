c061e5d9f0343f028c8095ddeaedf542bea82550
Casper <me@casperengelmann.com>
2019-10-24 19:28:10 +0200

    formatting + small reservation refactor + vue i18n

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,17 @@ use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\PaymentController\ReservationPaymentController;
 
 Auth::routes();
+Broadcast::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::prefix('cinema')->group(
     function () {
-        Route::get('/', [CinemaController::class, 'index'])->name('cinema.index');
-        Route::get('/{cinema}', [CinemaController::class, 'edit'])->name('cinema.edit');
+        Route::get('/', [CinemaController::class, 'index'])
+            ->name('cinema.index');
+
+        Route::get('/{cinema}', [CinemaController::class, 'edit'])
+            ->name('cinema.edit');
     }
 );
 
@@ -27,8 +31,11 @@ Route::prefix('film')->group(
 
         Route::prefix('anmeldelse')->group(
             function () {
-                Route::get('/{film}', [FilmRatingController::class, 'index'])->name('film.rating.index');
-                Route::post('/{film}', [FilmRatingController::class, 'store'])->name('film.rating.store');
+                Route::get('/{film}', [FilmRatingController::class, 'index'])
+                    ->name('film.rating.index');
+
+                Route::post('/{film}', [FilmRatingController::class, 'store'])
+                    ->name('film.rating.store');
             }
         );
     }
@@ -37,38 +44,75 @@ Route::prefix('film')->group(
 Route::prefix('visninger')->group(
     function () {
         Route::get('/', [ShowingController::class, 'pick'])->name('showing.pick');
+
         Route::get('/{date}', [ShowingController::class, 'index'])
             ->where(['date' => '\d{4}-\d{2}-\d{2}'])
             ->name('showing.index');
+            
         Route::get('/{date}/{showing}', [ShowingController::class, 'show'])
             ->where(['date' => '\d{4}-\d{2}-\d{2}'])
             ->name('showing.show');
-        Route::get('/{slug}', [ShowingController::class, 'days'])->name('showing.days');
+
+        Route::get('/{slug}', [ShowingController::class, 'days'])
+            ->name('showing.days');
     }
 );
 
 Route::prefix('reservation')->group(
     function () {
+        Route::get('/betal/{showing}', [
+            ReservationController::class,
+            'finalize',
+        ])->name('reservation.finalize');
+
         Route::get('/{reservation}', [
-            ReservationController::class, 'show'
+            ReservationController::class,
+            'show',
         ])->name('reservation.overview.show');
-        Route::post('/{date}/{showing}', [
-            ReservationController::class, 'store'
-        ])
-            ->where(['date' => '\d{4}-\d{2}-\d{2}'])
-            ->name('reservation.store');
+
+        Route::post('/{showing}', [
+            ReservationController::class,
+            'store',
+        ])->name('reservation.store');
 
         Route::get('/betal/{reservation}', [
-            ReservationPaymentController::class, 'index'
+            ReservationPaymentController::class,
+            'index',
         ])->name('reservation.payment.index');
+
         Route::get('/callback/{reservation}', [
-            ReservationPaymentController::class, 'callback'
+            ReservationPaymentController::class,
+            'callback',
         ])->name('reservation.payment.callback');
+
         Route::get('/betalt/{reservation}', [
-            ReservationPaymentController::class, 'success'
+            ReservationPaymentController::class,
+            'success',
         ])->name('reservation.payment.success');
+
         Route::get('/annulleret/{reservation}', [
-            ReservationPaymentController::class, 'cancel'
+            ReservationPaymentController::class,
+            'cancel',
         ])->name('reservation.payment.cancel');
     }
 );
+
+Route::get('/js/lang.js', function () {
+    $strings = cache()->remember('lang.js', now()->add(1, 'day'), function () {
+        $lang = config('app.locale');
+
+        $files   = glob(resource_path('lang/' . $lang . '/*.php'));
+        $strings = [];
+
+        foreach ($files as $file) {
+            $name           = basename($file, '.php');
+            $strings[$name] = require $file;
+        }
+
+        return $strings;
+    });
+
+    header('Content-Type: text/javascript');
+    echo('window.i18n = ' . json_encode($strings) . ';');
+    exit();
+})->name('assets.lang');
