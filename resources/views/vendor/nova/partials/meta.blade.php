08b4b157f5f7962886013615cb505c3f15dacf0e
Casper <me@casperengelmann.com>
2019-10-24 19:30:00 +0200

    added ziggy and laravel i18n

--- resources/views/vendor/nova/partials/meta.blade.php
+++ resources/views/vendor/nova/partials/meta.blade.php
@@ -1,2 +1,4 @@
 {{-- <link rel="icon" type="image/png" href="{{ asset('/img/favicon.png') }} "> --}}
 <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+<script src="/js/lang.js"></script>
+@routes
